8ef19bddfb566371d7caf76bf13f47988278805a
jmjubaer <jmjubaer3927@gmail.com>
2024-11-11 11:29:35 +0600

    Solve the first problem

---
+++ module/src/problem-1.ts
@@ -0,0 +1,11 @@
+type SumArray = (value: number[]) => number;
+const sumArray: SumArray = (array) => {
+    return array.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
+};
+
+const array = [1, 2, 3, 4, 5];
+const result = sumArray(array);
+console.log(result); // result is 15
